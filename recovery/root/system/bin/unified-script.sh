e55d1830ab89fbec2dbda9df64597ba882da993b
mauronofrio <poronofrio@gmail.com>
2020-09-14 22:24:38 +0200

    ginkgo: Add unified script

---
+++ recovery/root/system/bin/unified-script.sh
@@ -0,0 +1,39 @@
+#!/system/bin/sh
+# This script is needed to automatically set device props.
+
+
+load_willow()
+{
+    resetprop "ro.product.model" "Redmi Note 8T"
+    resetprop "ro.product.name" "willow"
+    resetprop "ro.build.product" "willow"
+    resetprop "ro.product.device" "willow"
+    resetprop "ro.vendor.product.device" "willow"
+}
+
+load_ginkgo()
+{
+    resetprop "ro.product.model" "Redmi Note 8"
+    resetprop "ro.product.name" "ginkgo"
+    resetprop "ro.build.product" "ginkgo"
+    resetprop "ro.product.device" "ginkgo"
+    resetprop "ro.vendor.product.device" "ginkgo"
+}
+
+
+project=$(getprop ro.boot.hwc)
+echo $project
+
+case $project in
+     "willow")
+         load_willow
+         ;;
+     "ginkgo")
+         load_ginkgo
+ 	;;
+     *)
+ 	load_ginkgo
+ 	;;
+esac
+
+exit 0
